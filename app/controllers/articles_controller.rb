e2c6954c82a9f41e5eb102a65962ce028e8d9c03
Vinleon Ang <javadripdev@gmail.com>
2023-03-07 21:58:35 +0800

    Wiki add index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  # before_action :set_article
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  # private
+
+  # def set_article
+  #   @article = Article.find(params[:id])
+  # end
+end
